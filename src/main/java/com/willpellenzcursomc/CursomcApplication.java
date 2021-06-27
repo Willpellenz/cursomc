76f96dcb38f215ab78622ed018db81364213eaf2
Wilian Pellenz <wilianpellenzc@hotmail.com>
2021-06-27 11:22:01 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/willpellenzcursomc/CursomcApplication.java
+++ src/main/java/com/willpellenzcursomc/CursomcApplication.java
@@ -8,13 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.willpellenzcursomc.domain.Categoria;
+import com.willpellenzcursomc.domain.Produto;
 import com.willpellenzcursomc.repositories.CategoriaRepository;
+import com.willpellenzcursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class cursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(cursomcApplication.class, args);
@@ -26,7 +30,19 @@ public class cursomcApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritorio");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat2));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
 	}
 
---
+++ src/main/java/com/willpellenzcursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.willpellenzcursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.willpellenzcursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
